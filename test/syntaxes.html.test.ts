ca460d51114a0d633842e4e9c2e8255b641a2ebc
Erik Lieben <erik.lieben@effectory.com>
2016-09-28 20:01:24 +0200

    fix(bindable): scope bindable attribute as bindable.attribute.html.au

--- test/syntaxes.html.test.ts
+++ test/syntaxes.html.test.ts
@@ -586,6 +586,20 @@ suite('The Aurelia HTML syntax', () => {
 
   }); 
 
+  test('must tokenize bindable attribute with scope "bindable.attribute.html.au"', () => {
+
+    // arrange
+    let scope = 'bindable.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template bindable="greeting,name">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 18);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
 });
 
 function tokenizeLine(line: string) {
