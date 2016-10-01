dbe598a0a34af3ec008d620d87d7f7b66806fbe2
Erik Lieben <erik.lieben@effectory.com>
2016-10-01 18:54:36 +0200

    refactor(view): move test to seperate file

--- test/syntaxes.html.test.ts
+++ test/syntaxes.html.test.ts
@@ -11,20 +11,6 @@ let grammar = registry.grammarForScopeName("au.html");
 
 suite('The Aurelia HTML syntax', () => {
 
-  test('must tokenize view attribute with scope "view.attribute.html.au"', () => {
-
-    // arrange
-    let scope = 'view.attribute.html.au';
-
-    // act
-    let lineToken = tokenizeLine('<div view.ref="foo">');
-
-    // assert
-    let token = getTokenOnCharRange(lineToken, 5, 9);
-    assert.equal(hasScope(token.scopes, scope), true);
-
-  });
-
   test('must tokenize controller attribute with scope "attribute.html.au"', () => {
 
     // arrange
