0af46302093d3d7217df42a52b6459f58e6228f5
Erik Lieben <erik.lieben@effectory.com>
2016-10-01 19:05:23 +0200

    refactor(bindable): move test to seperate file

---
+++ test/syntax.html/bindable.test.ts
@@ -0,0 +1,21 @@
+import * as assert from 'assert';
+import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
+
+suite('The Aurelia HTML syntax', () => {
+
+
+  test('must tokenize bindable attribute with scope "bindable.attribute.html.au"', () => {
+
+    // arrange
+    let scope = 'bindable.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template bindable="greeting,name">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 18);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
+});
