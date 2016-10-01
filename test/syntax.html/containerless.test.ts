d7c141086a997267e90438f63ba2f38530859e4b
Erik Lieben <erik.lieben@effectory.com>
2016-10-01 18:34:32 +0200

    refactor(containerless): move test to seperate file

---
+++ test/syntax.html/containerless.test.ts
@@ -0,0 +1,95 @@
+import * as assert from 'assert';
+import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
+
+suite('The Aurelia HTML syntax', () => {
+
+  test('must tokenize containerless attribute with scope "containerless.attribute.html.au"', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template containerless foo="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 23);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
+  test('must not tokenize containerless="" attribute', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template containerless="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 23);
+    assert.notEqual(token, null);
+    assert.equal(hasScope(token.scopes, scope), false);
+
+  });
+
+  test('must not tokenize containerless-foo="" attribute', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template containerless-foo="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 27);
+    assert.notEqual(token, null);
+    assert.equal(hasScope(token.scopes, scope), false);
+
+  });
+
+  test('must not tokenize foo-containerles="" attribute', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template foo-containerless="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 27);
+    assert.notEqual(token, null);
+    assert.equal(hasScope(token.scopes, scope), false);
+
+  });
+
+  test('must not tokenize foo-containerless="boo" attribute', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template foo-containerless="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 27);
+    assert.notEqual(token, null);
+    assert.equal(hasScope(token.scopes, scope), false);
+
+  });
+
+  test('must not tokenize containerlessfoo="boo" attribute', () => {
+
+    // arrange
+    let scope = 'containerless.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<template containerlessfoo="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 10, 26);
+    assert.notEqual(token, null);
+    assert.equal(hasScope(token.scopes, scope), false);
+
+  });
+});
+
---
+++ test/syntax.html/test.utils.ts
@@ -0,0 +1,21 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+let Registry = require(path.join(require.main.filename, '../../node_modules/vscode-textmate/release/main.js')).Registry;
+let registry: vscodeTextmate.Registry = new Registry();
+registry.loadGrammarFromPathSync('C:/Repository-Erik/Aurelia/vscode-extension/syntaxes/html.json');
+let grammar = registry.grammarForScopeName("au.html");
+
+export function tokenizeLine(line: string) {
+  return grammar.tokenizeLine(line, undefined);
+}
+
+export function getTokenOnCharRange(lineToken: vscodeTextmate.ITokenizeLineResult, startIndex: number, endIndex: number) {
+  let tokens = lineToken.tokens.filter(token => token.startIndex === startIndex && token.endIndex == endIndex);
+  return tokens.length === 1 ? tokens[0] : null;
+}
+
+export function hasScope(scopes: Array<string>, scope: string) {
+  let foundScopes = scopes.filter(s => s === scope);
+  return foundScopes.length === 1;
+}
