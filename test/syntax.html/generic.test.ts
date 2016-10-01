98056a47b0c4bc5624232fce062574d00e1fb887
Erik Lieben <erik.lieben@effectory.com>
2016-10-01 19:07:06 +0200

    refactor(generic html): move test to seperate file

--- test/syntax.html/generic.test.ts
+++ test/syntax.html/generic.test.ts
@@ -1,13 +1,5 @@
 import * as assert from 'assert';
-import * as vscode from 'vscode';
-import * as path from 'path';
-
-let reg = require(path.join(require.main.filename, '../../node_modules/vscode-textmate/release/main.js')).Registry;
-
-// arrange
-let registry: vscodeTextmate.Registry = new reg();
-registry.loadGrammarFromPathSync('C:/Repository-Erik/Aurelia/vscode-extension/syntaxes/html.json');
-let grammar = registry.grammarForScopeName("au.html");
+import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
 
 suite('The Aurelia HTML syntax', () => {
 
@@ -68,17 +60,3 @@ suite('The Aurelia HTML syntax', () => {
   });
   
 });
-
-function tokenizeLine(line: string) {
-  return grammar.tokenizeLine(line, undefined);
-}
-
-function getTokenOnCharRange(lineToken: vscodeTextmate.ITokenizeLineResult, startIndex: number, endIndex: number) {
-  let tokens = lineToken.tokens.filter(token => token.startIndex === startIndex && token.endIndex == endIndex);
-  return tokens.length === 1 ? tokens[0] : null;
-}
-
-function hasScope(scopes: Array<string>, scope: string) {
-  let foundScopes = scopes.filter(s => s === scope);
-  return foundScopes.length === 1;
-}
