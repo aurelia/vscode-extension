6e9b64dd323125daee7680545fc72867c0fdb266
Erik Lieben <erik.lieben@effectory.com>
2016-10-01 19:00:50 +0200

    refactor(if): move over rest of test cases

--- test/syntax.html/if.test.ts
+++ test/syntax.html/if.test.ts
@@ -31,4 +31,60 @@ suite('The Aurelia HTML syntax', () => {
 
   });
 
+ test('must tokenize if.bind with scope "databinding.html.au"', () => {
+
+    // arrange
+    let scope = 'databinding.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div if.bind="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 12);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
+  test('must tokenize if.ony-way with scope "databinding.html.au"', () => {
+
+    // arrange
+    let scope = 'databinding.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div if.one-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 15);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
+  test('must tokenize if.two-way with scope "databinding.html.au"', () => {
+
+    // arrange
+    let scope = 'databinding.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div if.two-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 15);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });
+
+  test('must tokenize if.one-time with scope "databinding.html.au"', () => {
+
+    // arrange
+    let scope = 'databinding.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div if.one-time="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 16);
+    assert.equal(hasScope(token.scopes, scope), true);
+
+  });  
+
 });
