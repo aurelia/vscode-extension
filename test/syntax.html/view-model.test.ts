b1109ef33112455c38445b3922127ab9c06c33d1
Erik Lieben <erik.lieben@effectory.com>
2016-10-12 21:59:23 +0200

    fix(syntax): correctly tokenize view-model attribute

--- test/syntax.html/view-model.test.ts
+++ test/syntax.html/view-model.test.ts
@@ -1,9 +1,65 @@
 import { assert } from 'chai';
 import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
 
-describe('The Aurelia HTML syntax view-model attribute', () => {
+describe(`The Aurelia HTML syntax view-model attribute`, () => {
 
-  it('must tokenize (view-model).x attribute with scope "attribute.html.au"', () => {
+  it(`must tokenize (view-model).bind attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div view-model.bind="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 15);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (view-model).one-way attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div view-model.one-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 15);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (view-model).two-way attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div view-model.two-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 15);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (view-model).one-time attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div view-model.one-time="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 15);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (view-model).ref attribute with scope "attribute.html.au"`, () => {
 
     // arrange
     let scope = 'view-model.attribute.html.au';
@@ -17,18 +73,60 @@ describe('The Aurelia HTML syntax view-model attribute', () => {
 
   });
 
-  it('must tokenize view-model attribute with scope "view-model.attribute.html.au"', () => {
+  it(`must tokenize (view-model)="foo" attribute with scope "view-model.attribute.html.au"`, () => {
 
     // arrange
     let scope = 'view-model.attribute.html.au';
 
     // act
-    let lineToken = tokenizeLine('<compose model.bind="item" view-model="widgets/${item.type}" />');
+    let lineToken = tokenizeLine('<compose model.bind="item" view-model="foo" />');
 
     // assert
     let token = getTokenOnCharRange(lineToken, 27, 37);
     assert.isOk(hasScope(token.scopes, scope));
 
   });
+
+  it(`must tokenize (view-model-foo)="foo" attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div view-model-foo="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 19);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });  
+
+  it(`must tokenize (foo-view-model)="foo" attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div foo-view-model="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 5, 19);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  }); 
+
+  it(`must tokenize a="(view-model)" attribute with scope "attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'view-model.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div a="view-model">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 18);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });    
 
 });
