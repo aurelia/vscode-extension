b6fdfa2b0f3d8d7f3d7f42c8770a06285700db3d
Erik Lieben <erik.lieben@effectory.com>
2016-10-13 08:18:49 +0200

    fix(syntax): correctly tokenize repeat.for attribute

--- test/syntax.html/repeat.for.test.ts
+++ test/syntax.html/repeat.for.test.ts
@@ -1,9 +1,9 @@
 import { assert } from 'chai';
 import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
 
-describe('The Aurelia HTML syntax repeat.for attribute', () => {
+describe(`The Aurelia HTML syntax repeat.for attribute`, () => {
 
-  it('must tokenize (repeat).for attribute with scope "repeat.attribute.html.au"', () => {
+  it(`must tokenize (repeat).for attribute with scope "repeat.attribute.html.au"`, () => {
 
     // arrange
     let scope = 'repeat.attribute.html.au';
@@ -17,7 +17,7 @@ describe('The Aurelia HTML syntax repeat.for attribute', () => {
 
   });
 
-  it('must tokenize repeat.(for) attribute with scope "for.attribute.html.au"', () => {
+  it(`must tokenize repeat.(for) attribute with scope "for.attribute.html.au"`, () => {
 
     // arrange
     let scope = 'for.attribute.html.au';
@@ -28,6 +28,48 @@ describe('The Aurelia HTML syntax repeat.for attribute', () => {
     // assert
     let token = getTokenOnCharRange(lineToken, 12, 15);
     assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize repeat.(for) attribute with scope "for.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'for.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div a="repeat.for">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 18);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize a="(repeat.for)" attribute with scope "for.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'for.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div a="repeat.for">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 18);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize a='(repeat.for)' attribute with scope "for.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'for.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<div a=\'repeat.for\'>');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 8, 18);
+    assert.isOk(!hasScope(token.scopes, scope));
 
   });
 
