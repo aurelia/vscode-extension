b9fa63e5eec87ded85ae80eee49c093a5a41a077
Erik Lieben <erik.lieben@effectory.com>
2016-10-24 10:57:38 +0200

    feat(syntax): tokenize compile-spy

---
+++ test/syntax.html/compile-spy.test.ts
@@ -0,0 +1,94 @@
+import { assert } from 'chai';
+import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
+
+describe('The Aurelia HTML syntax compile-spy attribute', () => {
+
+  it('must tokenize compile-spy attribute with scope "compile-spy.attribute.html.au"', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p compile-spy foo="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 14);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it('must not tokenize compile-spy="" attribute', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p compile-spy="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 14);
+    assert.isDefined(token);
+    assert.isNotOk(hasScope(token.scopes, scope));
+
+  });
+
+  it('must not tokenize compile-spy-foo="" attribute', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p compile-spy-foo="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 18);
+    assert.isDefined(token);
+    assert.isNotOk(hasScope(token.scopes, scope));
+
+  });
+
+  it('must not tokenize foo-containerles="" attribute', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p foo-compile-spy="">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 18);
+    assert.isDefined(token);
+    assert.isNotOk(hasScope(token.scopes, scope));
+
+  });
+
+  it('must not tokenize foo-compile-spy="boo" attribute', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p foo-compile-spy="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 18);
+    assert.isDefined(token);
+    assert.isNotOk(hasScope(token.scopes, scope));
+
+  });
+
+  it('must not tokenize compile-spyfoo="boo" attribute', () => {
+
+    // arrange
+    let scope = 'compile-spy.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<p compile-spyfoo="boo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 17);
+    assert.isDefined(token);
+    assert.isNotOk(hasScope(token.scopes, scope));
+
+  });
+});
