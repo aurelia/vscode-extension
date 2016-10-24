77b6828dc36700fc9b787e0e2a43a3e60a59d19f
Erik Lieben <erik.lieben@effectory.com>
2016-10-24 10:47:21 +0200

    feat(syntax): tokenize route-href

---
+++ test/syntax.html/route-href.test.ts
@@ -0,0 +1,160 @@
+import { assert } from 'chai';
+import { getTokenOnCharRange, hasScope, tokenizeLine, writeOut } from './test.utils';
+
+describe('The Aurelia HTML syntax route-href attribute', () => {
+
+  it(`must tokenize (route-href).bind="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href.bind="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 13);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (route-href).one-way="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href.one-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 13);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (route-href).two-way="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href.two-way="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 13);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (route-href).one-time="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href.one-time="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 13);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must tokenize (route-href)="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 13);
+    assert.isOk(hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize (route-href-foo)="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href-foo="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 17);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize (foo-route-href)="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a foo-route-href="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 17);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize (route-href-foo).bind="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a route-href-foo.bind="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 17);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize (foo-route-href).bind="foo" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a foo-route-href.bind="foo">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 3, 17);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });  
+
+  it(`must not tokenize a="route-href" attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a a="route-href">');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 6, 16);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+  it(`must not tokenize a='route-href' attribute with scope "route-href.attribute.html.au"`, () => {
+
+    // arrange
+    let scope = 'route-href.attribute.html.au';
+
+    // act
+    let lineToken = tokenizeLine('<a a=\'route-href\'>');
+
+    // assert
+    let token = getTokenOnCharRange(lineToken, 6, 16);
+    assert.isOk(!hasScope(token.scopes, scope));
+
+  });
+
+});
