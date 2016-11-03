eee039c90d16772912eb04c42f6d5b131daaa7e6
Eric Amodio <eric@amod.io>
2016-11-03 14:14:24 -0400

    feat(diagnostics): adds invalid casing diagnostic (#16)

--- dist/src/server/main.js
+++ dist/src/server/main.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
 const vscode_languageserver_1 = require('vscode-languageserver');
 const aureliaLanguageService_1 = require('./aurelia-languageservice/aureliaLanguageService');
 const languageModelCache_1 = require('./languageModelCache');
@@ -25,8 +33,13 @@ connection.onInitialize((params) => {
     };
 });
 let aureliaSettings;
-connection.onDidChangeConfiguration((change) => aureliaSettings = change.settings.aurelia);
+connection.onDidChangeConfiguration(change => aureliaSettings = change.settings.aurelia);
 let languageService = aureliaLanguageService_1.getLanguageService();
+documents.onDidChangeContent((change) => __awaiter(this, void 0, void 0, function* () {
+    let htmlDocument = htmlDocuments.get(change.document);
+    const diagnostics = yield languageService.doValidation(change.document, htmlDocument);
+    connection.sendDiagnostics({ uri: change.document.uri, diagnostics });
+}));
 connection.onCompletion(textDocumentPosition => {
     let document = documents.get(textDocumentPosition.textDocument.uri);
     let htmlDocument = htmlDocuments.get(document);
--- src/server/aurelia-languageservice/aureliaLanguageService.ts
+++ src/server/aurelia-languageservice/aureliaLanguageService.ts
@@ -1,17 +1,30 @@
 import { parse } from './parser/htmlParser';
 import { doComplete } from './services/htmlCompletion';
-import { TextDocument, Position, CompletionList } from 'vscode-languageserver-types';
+import { HTMLValidation } from './services/htmlValidation';
+import { CompletionList, Diagnostic, Position, TextDocument } from 'vscode-languageserver-types';
+import { HTMLDocument } from './parser/htmlParser';
 
-export declare type HTMLDocument = {};
+export { HTMLDocument } from './parser/htmlParser';
+
+export interface CompletionConfiguration {
+	[provider: string]: boolean;
+}
+
+export interface LanguageSettings {
+	validate?: boolean;
+}
 
 export interface LanguageService {
 	doComplete(document: TextDocument, position: Position, htmlDocument: HTMLDocument, quotes: string): CompletionList;
-  parseHTMLDocument(document: TextDocument): HTMLDocument;
+	doValidation(document: TextDocument, htmlDocument: HTMLDocument): Thenable<Diagnostic[]>;
+	parseHTMLDocument(document: TextDocument): HTMLDocument;
 }
 
 export function getLanguageService(): LanguageService {
+	const validation = new HTMLValidation();
 	return {
 		doComplete,
-    parseHTMLDocument: document => parse(document.getText()),
+		doValidation: validation.doValidation.bind(validation),
+		parseHTMLDocument: document => parse(document.getText())
 	};
 }
