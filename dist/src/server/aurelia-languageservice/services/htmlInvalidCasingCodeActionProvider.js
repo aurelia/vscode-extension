93ffd9695ae741ac85f3246cd0c26c1f3c3df1d3
Erik Lieben <erik@codetistic.com>
2017-01-31 22:04:39 +0100

    chore(gitignore): don't checkin generated files to repository

--- dist/src/server/aurelia-languageservice/services/htmlInvalidCasingCodeActionProvider.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-class GoCodeActionProvider {
-    provideCodeActions(document, range, context, token) {
-        console.log("CODE PRIVDER");
-        return null;
-    }
-}
-//# sourceMappingURL=htmlInvalidCasingCodeActionProvider.js.map
