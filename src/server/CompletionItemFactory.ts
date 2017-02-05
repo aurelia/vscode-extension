95ad4205dda75a1488beb53dbbf720e8c7a61cb8
Erik Lieben <erik@codetistic.com>
2017-02-05 20:59:17 +0100

    refactor data value completion

--- src/server/CompletionItemFactory.ts
+++ src/server/CompletionItemFactory.ts
@@ -54,7 +54,7 @@ export default class CompletionItemFactory {
             return this.createBindingCompletion(insideTag, text, positionNumber);
           case '"':
           case '\'':
-               return this.createValueCompletion(text, positionNumber);
+               return this.createValueCompletion(insideTag, text, positionNumber);
           default:
             return [];
         }
@@ -71,26 +71,26 @@ export default class CompletionItemFactory {
       }
   }
 
-  private elementMatch = /<([a-zA-Z-]*)\s([^<]*)$/;
-  private attributeMatch = /([a-zA-Z-]*)\.?([a-zA-Z-]*)?=['"]$/;
-  private attributeSplitterMatch = /(\S[^\. ]+)(\.(\S+))?=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g;
-
-
-  private createValueCompletion(text: string, position: number) {
+  private createValueCompletion(tag: TagDefinition, text: string, position: number) {
     let nextCharacter = text.substring(position, position + 1);
     if (/['"]/.test(nextCharacter)) {
-      let postText = text.substring(0, position);
-      let elementMatch = this.elementMatch.exec(postText);
-      let attributeMatch = this.attributeMatch.exec(postText);
-      if (elementMatch !== null && elementMatch.length) {
-        let elementName = elementMatch[1];
-        let attributeName = attributeMatch[1];
-        let bindingName = attributeMatch[2];
-        return this.attributeValueCompletionFactory.create(elementName, attributeName, bindingName);
+      let attribute;
+      let elementText = text.substring(tag.startOffset, tag.endOffset);
+      let tagPosition = position - tag.startOffset;
+      const attributeRegex = /([\w-]+)\.?\w*\=['"]/g;
+      let matches;
+      while (matches = attributeRegex.exec(elementText)) {
+        if (tagPosition >= matches.index && (tagPosition <= matches.index + matches[0].length)) {
+          let foundText = matches[1];
+          let attributes = tag.attributes.filter(a => a.name == foundText);
+          if (attributes.length) {
+            attribute = attributes[0];
+            break;
+          }
+        }  
       }
+      return this.attributeValueCompletionFactory.create(tag.name, attribute.name, attribute.binding);
     }
-
-    return null;
   }
 
   private createEmmetCompletion(text: string, position: number) {
