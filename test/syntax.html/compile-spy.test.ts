8e38b5d2a78732c2a333bab1f13e34c1a4cabde7
Erik Lieben <erik@codetistic.com>
2017-02-08 20:41:20 +0100

    refactor(compile-spy): remove unused writeOut import

--- test/syntax.html/compile-spy.test.ts
+++ test/syntax.html/compile-spy.test.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { getTokenOnCharRange, hasScope, tokenizeLine, writeOut } from './test.utils';
+import { getTokenOnCharRange, hasScope, tokenizeLine } from './test.utils';
 
 describe('The Aurelia HTML syntax compile-spy attribute', () => {
 
