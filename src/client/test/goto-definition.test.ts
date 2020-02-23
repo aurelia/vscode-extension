7fe88556cceb70613875f40c535470f31afcc336
hdn <hieudn5293@yahoo.de>
2020-02-23 22:57:39 +0100

    test(goto): more tests

--- src/client/test/goto-definition.test.ts
+++ src/client/test/goto-definition.test.ts
@@ -1,25 +1,57 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
+import * as path from 'path';
 import { getDocUri, activate } from './helper';
 
-describe('Should go to definition', () => {
-  const docUri = getDocUri('src/aurelia-components/hello-world.html');
+interface IExpectedResult {
+  expectedLocation: number[]
+  expectedFileName: string
+}
 
-  it('From View: Go to view model definition', async () => {
-    const position = new vscode.Position(5, 34);
-    await testCompletion(docUri, position, [7, 9, 7, 54]);
+describe('Go To Definition', () => {
+  const docUri = getDocUri('src/aurelia-components/compo-user.html');
 
-    const position2 = new vscode.Position(8, 34);
-    await testCompletion(docUri, position2, [4, 12, 4, 54]);
+  it('(xhdu5mWv) Should open`my-compo.ts` at eg. `public message: string;', async () => {
+    const position = new vscode.Position(26, 34);
+    await testCompletion(docUri, position, {
+      expectedLocation: [8, 9, 8, 54],
+      expectedFileName: 'compo-user.ts',
+    });
   });
+
+  it('(hEePRI08) Should open view model `compo-user.ts` at `class CompoUserCustomElement`', async () => {
+    const position = new vscode.Position(30, 28);
+    await testCompletion(docUri, position, {
+      expectedLocation: [5, 12, 5, 54],
+      expectedFileName: 'compo-user.ts',
+    })
+  });
+
+  it('(uOE33Pqm) Should open view model of `my-compo.ts`', async () => {
+    const position = new vscode.Position(7, 29);
+    await testCompletion(docUri, position, {
+      expectedLocation: [0, 0, 0, 6],
+      expectedFileName: 'my-compo.ts',
+    })
+  });
+
+  it('(EUmcjqaIO) Should open view model of `my-compo.ts`', async () => {
+    const position = new vscode.Position(19, 14);
+    await testCompletion(docUri, position, {
+      expectedLocation: [0, 0, 0, 6],
+      expectedFileName: 'my-compo.ts',
+    })
+  });
+
 });
 
 async function testCompletion(
   docUri: vscode.Uri,
   position: vscode.Position,
-  expectedLocation: number[],
+  expectedResult: IExpectedResult
 ) {
   await activate(docUri);
+  const { expectedFileName, expectedLocation } = expectedResult;
 
   const definitionLocation = (await vscode.commands.executeCommand(
     'vscode.executeDefinitionProvider',
@@ -27,8 +59,13 @@ async function testCompletion(
     position
   )) as vscode.Location[];
 
-  assert.equal(definitionLocation[0].range.start.line, expectedLocation[0]);
-  assert.equal(definitionLocation[0].range.start.character, expectedLocation[1]);
-  assert.equal(definitionLocation[0].range.end.line, expectedLocation[2]);
-  assert.equal(definitionLocation[0].range.end.character, expectedLocation[3]); // Why 54? Because in `hello-world.ts`, the end line should be 41ish
+  const targetDefinition = definitionLocation[0];
+  const { range, uri } = targetDefinition
+
+  assert.equal(range.start.line, expectedLocation[0]);
+  assert.equal(range.start.character, expectedLocation[1]);
+  assert.equal(range.end.line, expectedLocation[2]);
+  assert.equal(range.end.character, expectedLocation[3]); // Why 54? Because in `hello-world.ts`, the end line should be 41ish
+
+  assert.equal(path.basename(uri.path), expectedFileName);
 }
