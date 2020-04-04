8a32c96374e1ffb7c9e4557639bb28b9dbc81bb4
Per Kristian Kummermo <per.kristian.kummermo@gmail.com>
2020-04-05 00:53:21 +0200

    feat(file-watching): add documentation and refresh of web components (#122)
    * chore(package.json): add typescript as dep and rename categories
    
    * feat(file-watching): add file watching to extension
    
    This includes documentation for the custom components as well as
    allowing us to watch for changes when the custom component
    changes in terms of edit and creation. Using a TS Program
    and a watchCompilerHost (while bypassing the compile), we can get
    an incremental change handler for ts files in the project using
    the tsconfig.json (if available).
    
    * refactor(config): use ext tsconfig is none found
    
    Co-Authored-By: hiaux0 <30693990+hiaux0@users.noreply.github.com>
    
    * fix(autocomplete.docs): const -> let
    
    Co-authored-by: hiaux0 <30693990+hiaux0@users.noreply.github.com>

--- src/server/Completions/CustomElementCompletionFactory.ts
+++ src/server/Completions/CustomElementCompletionFactory.ts
@@ -1,11 +1,15 @@
 import {
   CompletionItem,
   CompletionItemKind,
-  InsertTextFormat, MarkedString
+  InsertTextFormat, MarkupKind, createConnection, IConnection
 } from 'vscode-languageserver';
+import * as ts from 'typescript';
 import { autoinject } from 'aurelia-dependency-injection';
 import { AureliaApplication } from "../FileParser/Model/AureliaApplication";
 import ElementLibrary from './Library/_elementLibrary';
+
+const connection: IConnection = createConnection();
+const logger = connection.console;
 
 /**
  * Get all the Custom elements from (based on aureliaApplication), and provide completion
@@ -13,22 +17,54 @@ import ElementLibrary from './Library/_elementLibrary';
 @autoinject()
 export default class CustomElementCompletionFactory {
 
-  constructor(private readonly library: ElementLibrary) { }
+  public constructor(private readonly library: ElementLibrary) { }
 
   public create(parent: string, aureliaApplication: AureliaApplication): CompletionItem[] {
-
     const result: CompletionItem[] = [];
 
     // Assumption: Every custom element has a script and a template
     const customElements = aureliaApplication.components.filter(component => component.paths.length >= 2);
-    customElements.forEach(element => {
+
+    // Map documentation to file paths if found
+    const fileClassDocumentation: {[fileName: string]: string} = {};
+
+    if (aureliaApplication.getProgram() !== undefined) {
+      const checker = aureliaApplication.getProgram().getTypeChecker();
+      for (const sourceFile of aureliaApplication.getProgram().getSourceFiles()) {
+        if (!sourceFile.isDeclarationFile) {
+          sourceFile.forEachChild((node) => {
+            if (ts.isClassDeclaration(node)) {
+              const symbol = checker.getSymbolAtLocation(node.name);
+              fileClassDocumentation[sourceFile.fileName] = ts.displayPartsToString(
+                symbol.getDocumentationComment(checker));
+            }
+          });
+        }
+      }
+    } else {
+      logger.log("CustomElement: Program missing, can't fetch documentation.");
+    }
+
+    customElements.forEach(customElement => {
+      let documentation = `\`${customElement.name}\``;
+
+      // Check if there's documentation for the file. If yes use it, otherwise fallback to documentation.
+      customElement.paths.forEach((path) => {
+        if (path.endsWith('.ts') && fileClassDocumentation[path] !== "" && fileClassDocumentation[path] !== undefined) {
+            documentation = fileClassDocumentation[path];
+        }
+      });
+
       result.push({
-        documentation: MarkedString.fromPlainText(`${element.name}`).toString(),
-        detail: `Custom Element`,
-        insertText: `${element.name}$2>$1</${element.name}>$0`,
+        documentation: {
+          kind: MarkupKind.Markdown,
+          value: documentation,
+        },
+        detail: `Au Custom Element`,
+        insertText: `${customElement.name}$2>$1</${customElement.name}>$0`,
         insertTextFormat: InsertTextFormat.Snippet,
         kind: CompletionItemKind.Property,
-        label: `Custom Element: ${element.name}`,
+        label: `Au Custom Element: ${customElement.name}`,
       });
     });
 
--- src/server/FileParser/Model/WebComponent.ts
+++ src/server/FileParser/Model/WebComponent.ts
@@ -2,14 +2,14 @@ import { HtmlTemplateDocument } from './HtmlTemplateDocument';
 import { ViewModelDocument } from './ViewModelDocument';
 
 export class WebComponent {
-  constructor(public name: string) {
+  public constructor(public name: string) {
 
   }
 
   public document: HtmlTemplateDocument;
   public viewModel: ViewModelDocument;
 
-  public paths = [];
+  public paths: string[] = [];
 
   public classes = [];
 }
