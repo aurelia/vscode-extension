68762d0ddfbcefc6163a9e556b511a176311b273
Per Kristian Kummermo <per.kristian@kummermo.no>
2020-04-05 23:36:33 +0200

    chore(lint): lint AttributeCompletionFactory before rewrite

--- src/server/Completions/AttributeCompletionFactory.ts
+++ src/server/Completions/AttributeCompletionFactory.ts
@@ -3,7 +3,7 @@ import {
   CompletionItem,
   CompletionItemKind,
   InsertTextFormat,
-  MarkedString
+  MarkupKind,
 } from 'vscode-languageserver';
 import { autoinject } from 'aurelia-dependency-injection';
 import ElementLibrary from './Library/_elementLibrary';
@@ -14,7 +14,7 @@ import AureliaSettings from "../AureliaSettings";
 @autoinject()
 export default class AureliaAttributeCompletionFactory extends BaseAttributeCompletionFactory {
 
-  constructor(library: ElementLibrary, private readonly settings: AureliaSettings) { super(library); }
+  public constructor(library: ElementLibrary, private readonly settings: AureliaSettings) { super(library); }
 
   public create(elementName: string, existingAttributes: string[], aureliaApplication: AureliaApplication): CompletionItem[] {
 
@@ -27,7 +27,7 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
       this.addAttributes(GlobalAttributes.attributes, result, existingAttributes, this.settings.quote);
     }
 
-    if (element.attributes) {
+    if (element.attributes !== undefined) {
       this.addAttributes(element.attributes, result, existingAttributes, this.settings.quote);
     }
 
@@ -35,7 +35,7 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
       this.addEvents(GlobalAttributes.events, result, existingAttributes, this.settings.quote);
     }
 
-    if (element.events) {
+    if (element.events !== undefined) {
       this.addEvents(element.events, result, existingAttributes, this.settings.quote);
     }
 
@@ -45,30 +45,29 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
   /**
    * Look at the View Model and provide all class variables as completion in kebab case.
    * TODO: Only bindables should be provided.
-   *
-   * @param result
-   * @param elementName
-   * @param aureliaApplication
    */
   private addViewModelBindables(result: CompletionItem[], elementName: string, aureliaApplication: AureliaApplication) {
-    if (aureliaApplication.components) {
+    if (aureliaApplication.components.length > 0) {
       aureliaApplication.components.forEach(component => {
-        if (component.name !== elementName) return;
-        if (!component.viewModel) return;
-        if (!component.viewModel.properties) return;
+        if (component.name !== elementName || component.viewModel?.properties === undefined) return;
 
         component.viewModel.properties.forEach(property => {
           if (!property.isBindable) return;
 
+          const documentation = `Bindable type: \`${property.type}\``;
+
           const quote = this.settings.quote;
           const varAsKebabCase = property.name.split(/(?=[A-Z])/).map(s => s.toLowerCase()).join('-');
           result.push({
-            documentation: property.type,
-            detail: 'View Model Bindable',
-            insertText: `${varAsKebabCase}.$\{1:bind\}=${quote}$\{0:${property.name}\}${quote}`,
+            documentation: {
+              kind: MarkupKind.Markdown,
+              value: documentation
+            },
+            detail: 'Au View Model Bindable',
+            insertText: `${varAsKebabCase}.$\{1:bind}=${quote}$\{0:${property.name}}${quote}`,
             insertTextFormat: InsertTextFormat.Snippet,
             kind: CompletionItemKind.Variable,
-            label: `View Model: ${varAsKebabCase}`
+            label: `Au View Model: ${varAsKebabCase}`
           });
         });
       });
