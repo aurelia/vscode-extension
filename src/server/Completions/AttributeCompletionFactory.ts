9740d79d2460716867e5e5b5bd8b4ce8a0a1ec64
Per Kristian Kummermo <per.kristian@kummermo.no>
2020-04-07 21:55:09 +0200

    feat(attributeCompletion): replaced attribute completion (#128)
    * chore(lint): lint AttributeCompletionFactory before rewrite
    
    * refactor(webcomponent): type attribute classes
    
    * chore(main): rename component iteration variable
    
    * fix(eslintrc): use 0 instead of 'off' to disable rule
    
    * feat(js-support): add support for js if allowJS is true in tsconfig
    
    * feat(attributeCompletion): replaced attribute completion with program and added doc support
    
    * refactor(AttributeCompletion): use functions for more complex element name mappings
    
    * refactor(readability): use only variables for interpolation and create stringAsKebabCase

--- src/server/Completions/AttributeCompletionFactory.ts
+++ src/server/Completions/AttributeCompletionFactory.ts
@@ -3,18 +3,25 @@ import {
   CompletionItem,
   CompletionItemKind,
   InsertTextFormat,
-  MarkedString
+  MarkupKind,
+  IConnection,
+  createConnection,
 } from 'vscode-languageserver';
 import { autoinject } from 'aurelia-dependency-injection';
 import ElementLibrary from './Library/_elementLibrary';
 import { GlobalAttributes } from './Library/_elementStructure';
 import BaseAttributeCompletionFactory from './BaseAttributeCompletionFactory';
 import AureliaSettings from "../AureliaSettings";
+import * as ts from 'typescript';
+import * as Path from 'path';
+
+const connection: IConnection = createConnection();
+const logger = connection.console;
 
 @autoinject()
 export default class AureliaAttributeCompletionFactory extends BaseAttributeCompletionFactory {
 
-  constructor(library: ElementLibrary, private readonly settings: AureliaSettings) { super(library); }
+  public constructor(library: ElementLibrary, private readonly settings: AureliaSettings) { super(library); }
 
   public create(elementName: string, existingAttributes: string[], aureliaApplication: AureliaApplication): CompletionItem[] {
 
@@ -27,7 +34,7 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
       this.addAttributes(GlobalAttributes.attributes, result, existingAttributes, this.settings.quote);
     }
 
-    if (element.attributes) {
+    if (element.attributes !== undefined) {
       this.addAttributes(element.attributes, result, existingAttributes, this.settings.quote);
     }
 
@@ -35,7 +42,7 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
       this.addEvents(GlobalAttributes.events, result, existingAttributes, this.settings.quote);
     }
 
-    if (element.events) {
+    if (element.events !== undefined) {
       this.addEvents(element.events, result, existingAttributes, this.settings.quote);
     }
 
@@ -44,34 +51,104 @@ export default class AureliaAttributeCompletionFactory extends BaseAttributeComp
 
   /**
    * Look at the View Model and provide all class variables as completion in kebab case.
-   * TODO: Only bindables should be provided.
-   *
-   * @param result
-   * @param elementName
-   * @param aureliaApplication
    */
   private addViewModelBindables(result: CompletionItem[], elementName: string, aureliaApplication: AureliaApplication) {
-    if (aureliaApplication.components) {
-      aureliaApplication.components.forEach(component => {
-        if (component.name !== elementName) return;
-        if (!component.viewModel) return;
-        if (!component.viewModel.properties) return;
+    const program = aureliaApplication.getProgram();
+    if (program !== undefined) {
+      const checker = program.getTypeChecker();
+      for (const sourceFile of program.getSourceFiles()) {
+        // Fetch name of source file and see if it matches the custom component
+        if (!sourceFile.isDeclarationFile && this.getElementNameFromSourceFile(sourceFile) === elementName) {
+          sourceFile.forEachChild((node) => {
+            // Check whether it's a class declaration and that the class name matches the custom component
+            if (ts.isClassDeclaration(node) && this.getElementNameFromClassDeclaration(node) === elementName) {
+              node.members.forEach(member => {
+                if (ts.isPropertyDeclaration(member) && this.propertyIsBindable(member)) {
+                  const propertyName = member.name?.getText();
 
-        component.viewModel.properties.forEach(property => {
-          if (!property.isBindable) return;
+                  // Get bindable type. If bindable type is undefined, we set it to be "unknown".
+                  const bindableType = member.type?.getText() !== undefined ? member.type?.getText() : "unknown";
+                  const bindableTypeText = `Bindable type: \`${bindableType}\``;
 
-          const quote = this.settings.quote;
-          const varAsKebabCase = property.name.split(/(?=[A-Z])/).map(s => s.toLowerCase()).join('-');
-          result.push({
-            documentation: property.type,
-            detail: 'View Model Bindable',
-            insertText: `${varAsKebabCase}.$\{1:bind\}=${quote}$\{0:${property.name}\}${quote}`,
-            insertTextFormat: InsertTextFormat.Snippet,
-            kind: CompletionItemKind.Variable,
-            label: `View Model: ${varAsKebabCase}`
+                  // Add comment documentation if available
+                  const symbol = checker.getSymbolAtLocation(member.name);
+                  const commentDoc = ts.displayPartsToString(
+                    symbol.getDocumentationComment(checker)
+                  );
+
+                  // Add default values. The value can be undefined, but that is correct in most cases.
+                  const defaultValue = member.initializer?.getText();
+                  const defaultValueText = `Default value: \`${defaultValue}\``;
+
+                  // Concatenate documentation parts with spacing
+                  const documentation = `${commentDoc}\n\n${bindableTypeText}\n\n${defaultValueText}`;
+
+                  const quote = this.settings.quote;
+                  const varAsKebabCase = this.stringAsKebabCase(propertyName);
+                  result.push({
+                    documentation: {
+                      kind: MarkupKind.Markdown,
+                      value: documentation
+                    },
+                    detail: `${varAsKebabCase}`,
+                    insertText: `${varAsKebabCase}.$\{1:bind}=${quote}$\{0:${propertyName}}${quote}`,
+                    insertTextFormat: InsertTextFormat.Snippet,
+                    kind: CompletionItemKind.Variable,
+                    label: `${varAsKebabCase} (Au Bindable)`
+                  });
+                }
+              });
+            }
           });
-        });
-      });
+        }
+      }
+    } else {
+      logger.log("AttributeCompletion bindables: Program missing, can't fetch CompletionItems.");
     }
+  }
+
+  /**
+   * propertyIsBindable checks whether a PropertyDeclaration is bindable
+   *
+   * @param propertyDeclaration - PropertyDeclaration to check if is bindable
+   */
+  private propertyIsBindable(propertyDeclaration: ts.PropertyDeclaration): boolean {
+    return propertyDeclaration.decorators?.some(decorator => { return decorator.getText().includes("@bindable"); });
+  }
+
+  /**
+   * Fetches the equivalent component name based on the given ts.SourceFile
+   *
+   * @param sourceFile - The source file to map a component name from
+   */
+  private getElementNameFromSourceFile(sourceFile: ts.SourceFile): string {
+    // Get basename and strip valid extensions
+    const pathName = Path.basename(sourceFile.fileName)
+      .replace(/\.(ts|js|html)$/, '');
+
+    return this.stringAsKebabCase(pathName);
+  }
+
+  /**
+   * Fetches the equivalent component name based on the given class declaration
+   *
+   * @param sourceFile - The class declaration to map a component name from
+   */
+  private getElementNameFromClassDeclaration(classDeclaration: ts.ClassDeclaration): string {
+    return this.stringAsKebabCase(classDeclaration.name?.getText());
+  }
+
+  /**
+   * stringAsKebabCase converts a string to kebab case. This is useful when trying to get element name from strings.
+   *
+   * Ex: HelloWorld => hello-world
+   *
+   * @param candidate - The string to be converted to kebab case
+   */
+  private stringAsKebabCase(candidate: string): string {
+    return candidate
+      .split(/(?=[A-Z])/)
+      .map(s => s.toLowerCase())
+      .join('-');
   }
 }
