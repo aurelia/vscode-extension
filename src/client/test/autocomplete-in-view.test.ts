fb8c767ed7c427167bf672fa30c10d81e9d0ba03
Per Kristian Kummermo <per.kristian@kummermo.no>
2020-04-08 16:14:02 +0200

    refactor(vscode): remove old e2 vscode dependency and migrate to new (#130)
    * refactor(vscode): remove old e2 vscode dependency and migrate to new. Fixes #129
    
    * chore(eslint): refactor according to eslint

--- src/client/test/autocomplete-in-view.test.ts
+++ src/client/test/autocomplete-in-view.test.ts
@@ -1,18 +1,19 @@
 import * as vscode from 'vscode';
 import * as assert from 'assert';
-import * as path from 'path';
 import { getDocUri, activate } from './helper';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { suite, test } from 'mocha';
 
 interface ExpectedResult {
   label: string;
   expectedNames: string[];
 }
 
-describe('Completion In View', () => {
+suite('Completion In View', () => {
   const docUri = getDocUri('src/aurelia-components/autocomplete-in-view/autocomplete-in-view.html');
 
   /** Custom Element: my-compo */
-  it('(hmtMmuEY) Should have completion for custom elements', async () => {
+  test('(hmtMmuEY) Should have completion for custom elements', async () => {
     const position = new vscode.Position(5, 2);
     await testCompletion(docUri, position, {
       label: 'Custom Element: ',
@@ -25,7 +26,7 @@ describe('Completion In View', () => {
   });
 
   // View Model: string-bindable
-  it('(aqwmpQ1H) Should have completion for bindables of `my-compo.ts`', async () => {
+  test('(aqwmpQ1H) Should have completion for bindables of `my-compo.ts`', async () => {
     const position = new vscode.Position(9, 12);
     await testCompletion(docUri, position, {
       label: 'View Model: ',
@@ -38,7 +39,7 @@ describe('Completion In View', () => {
     });
   });
 
-  it('(Iku78qwA) Should have completion for `autocomplete-in-view.ts` methods', async () => {
+  test('(Iku78qwA) Should have completion for `autocomplete-in-view.ts` methods', async () => {
     const position = new vscode.Position(21, 4);
     await testCompletion(docUri, position, {
       label: 'View Model: ',
@@ -69,9 +70,9 @@ async function testCompletion(
   });
 
   matchingResults.forEach(matchingItem => {
-    const isMatch = !!expectedNames.find(expectedName => {
+    const isMatch = expectedNames.find(expectedName => {
       return matchingItem.label === `${label}${expectedName}`;
-    });
+    }) !== undefined;
     assert.strictEqual(isMatch, true);
   });
 }
--- src/client/test/index.ts
+++ src/client/test/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Copied from https://github.com/microsoft/vscode-extension-samples/blob/master/lsp-sample/client/src/test/index.ts
 
 /* --------------------------------------------------------------------------------------------
@@ -5,12 +6,48 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as testRunner from 'vscode/lib/testrunner';
+import * as path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as Mocha from 'mocha';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as glob from 'glob';
 
-testRunner.configure({
-  ui: 'bdd',
-  useColors: true,
-  timeout: 100000
-});
+export async function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'bdd',
+  });
+  mocha.useColors(true);
+  mocha.timeout(100000);
 
-module.exports = testRunner;
+  // eslint-disable-next-line no-undef
+  const testsRoot = __dirname;
+
+  return new Promise((resolve, reject) => {
+    glob('**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err !== null) {
+        // eslint-disable-next-line no-undef
+        console.error("Got error from glob", err);
+        return reject(err);
+      }
+
+      // Add files to the test suite
+      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run(failures => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`));
+          } else {
+            resolve();
+          }
+        });
+      } catch (mochaErr) {
+        // eslint-disable-next-line no-undef
+        console.error(mochaErr);
+        reject(mochaErr);
+      }
+    });
+  });
+}
