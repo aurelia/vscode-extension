00c9de285658b25340fad4fc1b88fa1e310c93e3
hdn <hieudn5293@yahoo.de>
2021-09-26 14:53:30 +0200

    🔧 chore(jest): config sucrase

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  modulePaths: ['<rootDir>'],
-  moduleDirectories: ['node_modules', '<rootDir>/server/node_modules'],
-  preset: 'ts-jest',
-  verbose: true,
-  testRegex: '.(test|spec|steps)\\.ts$',
-  coverageDirectory: '.coverage',
-  coverageReporters: ['text', 'text-summary'],
-  coverageThreshold: {
-    global: { statements: 90, lines: 90, functions: 90 },
+  roots: ['<rootDir>/tests'],
+  // modulePaths: ['<rootDir>'],
+  // moduleDirectories: ['node_modules', '<rootDir>/server/node_modules'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': '@sucrase/jest-plugin',
   },
-  testPathIgnorePatterns: ['/build/', '/node_modules/'],
+  // preset: 'ts-jest',
+  // verbose: true,
+  testRegex: '.(test|spec|steps).[j,t]s$',
+  // coverageDirectory: '.coverage',
+  // coverageReporters: ['text', 'text-summary'],
+  // coverageThreshold: {
+  //   global: { statements: 90, lines: 90, functions: 90 },
+  // },
+  testPathIgnorePatterns: ['/build/', '/node_modules/', '/testFixture/'],
   globals: {
     'ts-jest': {
       tsconfig: 'tests/tsconfig.json',
