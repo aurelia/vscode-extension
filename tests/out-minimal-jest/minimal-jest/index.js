a79676bba8ea62647ced49399ad08bfb73898577
hdn <hieudn5293@yahoo.de>
2021-09-29 15:30:14 +0200

    remove out

--- tests/out-minimal-jest/minimal-jest/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: !0
-}), exports.add = void 0;
-var add = function(a, b) {
-    return a + b;
-};
-exports.add = add;
--- tests/out-minimal-jest/minimal-jest/index.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var ref = require("./index"), add = ref.add;
-describe("add", function() {
-    it("should return 2 when it gives 1,1", function() {
-        expect(add(1, 1)).toBe(2);
-    });
-});
