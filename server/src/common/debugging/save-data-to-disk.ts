0dfa3dab9c2c034a6b2e7768d6e1908c13b9e66a
hdn <hieudn5293@yahoo.de>
2021-10-08 15:57:07 +0200

    debug(): add debugging helpers

---
+++ server/src/common/debugging/save-data-to-disk.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+
+const SAVE_FILE_PATH =
+  '/Users/hdn/Desktop/aurelia-vscode-extension/vscode-extension/server/src/common/debugging/debugging-data.json';
+
+export function saveDataToDisk(data: Record<string, any>): void {
+  let finalData = {};
+
+  if (fs.existsSync(SAVE_FILE_PATH)) {
+    const existingSave = fs.readFileSync(SAVE_FILE_PATH, 'utf-8');
+    const asJson = JSON.parse(existingSave || '{}');
+    finalData = asJson;
+  }
+
+  finalData = {
+    ...finalData,
+    ...data,
+  };
+  const asJsonString = JSON.stringify(finalData, null, 4);
+  fs.writeFileSync(SAVE_FILE_PATH, asJsonString);
+}
+
+export function getSaveData(): Record<string, any> {
+  if (fs.existsSync(SAVE_FILE_PATH)) {
+    const existingSave = fs.readFileSync(SAVE_FILE_PATH, 'utf-8');
+    const asJson = JSON.parse(existingSave);
+    return asJson;
+  }
+
+  throw new Error('No Save data');
+}
