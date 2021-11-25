f33d306674d20ee58a9160e3e053ef4f8825397f
hiaux0 <hieudn5293@yahoo.de>
2021-11-25 18:59:28 +0100

    chore(): allow project.config.ts

---
+++ server/src/project.config.ts
@@ -0,0 +1,8 @@
+export const PROJECT_CONFIG = {
+  /**
+   * Path to the vscode-extension project
+   *
+   * @example ~/coding/repos/vscode-extension
+   */
+  projectPath: 'C:\\Users\\hdn local\\Desktop\\dev\\aurelia\\vscode-extension',
+};
