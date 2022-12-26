5bd6c5bc64aa0149fd0e5d53e430d1a31b2621b6
hdn <hieudn5293@yahoo.de>
2022-12-26 13:25:06 +0100

    feat(commands.extract): validate user input

--- client/src/feature/userInput/userInput.ts
+++ client/src/feature/userInput/userInput.ts
@@ -1,16 +1,15 @@
-import { window, commands, ExtensionContext } from 'vscode';
-import { CREATE_COMPONEN_TEXT } from '../../shared/constants';
-import { GetComponentState } from '../../shared/types/types';
-import { multiStepInput } from './multiStepInput';
+import { window, ExtensionContext } from 'vscode';
 
 export async function getUserInputCommand(context: ExtensionContext) {
   const result = await window.showInputBox({
-		// value: 'abcdef',
-		// valueSelection: [2, 4],
-		placeHolder: 'Enter component name',
-		// validateInput: text => {
-		// 	return text === '123' ? 'Not 123!' : null;
-		// }
-	});
-  return result
+    // value: 'abcdef',
+    // valueSelection: [2, 4],
+    placeHolder: 'Enter component name',
+    validateInput: (text) => {
+      const isEmpty = text === '';
+      const isTooShort = text.length <= 2;
+      if (isEmpty || isTooShort) return 'Must contain at least 3 characters';
+    },
+  });
+  return result;
 }
